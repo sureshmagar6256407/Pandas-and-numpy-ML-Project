45c257ad34257c74dd4f259946888434ecf29290
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-05-28 12:10:38 +0000

    learning about mean , medium and mode for machine leraning here

--- ml_prcatice/first.py
+++ ml_prcatice/first.py
@@ -1,1 +1,9 @@
-name  = if (name == "suresh pun magar")
+# import numpy   
+# from scipy import stats
+# speed  = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+# x    = numpy.mean(speed)
+# print(x)
+# y   = numpy.median(speed)
+# print(f"the speed of  median is {y}")
+# z  = stats.mode(speed)
+# print(f"the speed of mode is {z}")
