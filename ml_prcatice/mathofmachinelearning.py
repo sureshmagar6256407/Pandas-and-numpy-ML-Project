781dae81778a2e73787521b076812f8da99b6025
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-05-28 13:28:48 +0000

    The Matplotlib module has a method for drawing scatter plots, it needs two arrays of the same length, one for the values of the x-axis, and one for the values of the y-axis:

--- ml_prcatice/mathofmachinelearning.py
+++ ml_prcatice/mathofmachinelearning.py
@@ -61,6 +61,13 @@ import matplotlib.pyplot as plt
 
 
 #NORMAL DISTRIBUTION
-x = numpy.random.normal(5.0,1.0,100000)
-plt.hist(x,100)
-plt.show()
+# x = numpy.random.normal(5.0,1.0,100000)
+# plt.hist(x,100)
+# plt.show()
+
+
+# #Scatter plot   
+# x = numpy.random.normal(5.0 ,1.0 ,1000)
+# y  = numpy.random.normal(10.0,2.0,1000)  
+# plt.scatter(x,y)
+# plt.show()
