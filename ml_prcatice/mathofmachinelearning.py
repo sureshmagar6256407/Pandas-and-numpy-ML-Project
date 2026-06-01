5eea4a7d7d478ce161a9afc9a931df9458a2357d
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-06-01 09:14:21 +0000

    linear regression doin in normal formula using math

--- ml_prcatice/mathofmachinelearning.py
+++ ml_prcatice/mathofmachinelearning.py
@@ -77,6 +77,7 @@ from scipy import stats
 
 # LINEAR REGRESSION
 # 1. Class define gareko
+"""
 class LinearRegressionModel:
     def __init__(self, x_data, y_data):
         self.x = x_data
@@ -129,3 +130,17 @@ def first_analysis() :
     prediction = my_model_object.predict_new(naya_value)
     print(f"\n[Prediction Result] If X = {naya_value}, Predicted Y = {prediction:.2f}")
 first_analysis()
+"""
+
+""""""
+import matplotlib.pyplot as plt 
+from scipy import stats
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+slope , inetercept,r ,p,std_err = stats.linregress(x,y)
+def myfunc(x) : 
+    return slope * x +inetercept  
+mymodel  = list(map(myfunc,x))
+plt.scatter(x,y)
+plt.plot(x,mymodel)
+plt.show()
