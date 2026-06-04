af227fc72db494648fc254b85e55b185ead7cb29
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-06-04 05:33:13 +0000

    multi-dimensional

--- ml_prcatice/num_py.py
+++ ml_prcatice/num_py.py
@@ -6,6 +6,17 @@ numpy_array  = np.array([1,2,3,4])
 print(numpy_array)
 """
 
+
+"""
+# two dimensional array
 arr_2d = np.array([[1,2,3],
                    [4,5,6]])
 print(arr_2d)
+"""
+
+"""
+# three dimensional array
+"""
+multi_dimensional  = np.array([[[1,2,3],
+                                [4,5,6]]])
+print(multi_dimensional)
