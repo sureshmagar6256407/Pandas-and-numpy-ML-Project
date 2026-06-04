59d87618cb768dbba3ef9d360ea288dda7c3e4f8
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-06-04 05:29:50 +0000

    2dimmensional aray

--- ml_prcatice/num_py.py
+++ ml_prcatice/num_py.py
@@ -1,3 +1,11 @@
-import numpy as np  
+import numpy as np
+
+"""
+# one dimensional array
 numpy_array  = np.array([1,2,3,4])
 print(numpy_array)
+"""
+
+arr_2d = np.array([[1,2,3],
+                   [4,5,6]])
+print(arr_2d)
